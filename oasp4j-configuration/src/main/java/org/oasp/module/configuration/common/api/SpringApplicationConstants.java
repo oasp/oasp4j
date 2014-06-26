31de6dd2841895d79a54055df8ba2f22e4950219
Joerg Hohwiller <joerg@j-hohwiller.de>
2014-06-26 16:12:57 +0200

    #9: changed configuration layout

--- oasp4j-configuration/src/main/java/org/oasp/module/configuration/common/api/SpringApplicationConstants.java
+++ oasp4j-configuration/src/main/java/org/oasp/module/configuration/common/api/SpringApplicationConstants.java
@@ -1,26 +1,26 @@
 package org.oasp.module.configuration.common.api;
 
 /**
- * Central constants for spring configuration files. Mainly for testing but also for productive usage.
+ * Central constants for spring configuration files. Mainly for testing but may also be used for productive usage.
  * 
  * @author hohwille
  */
 public final class SpringApplicationConstants {
 
   /** Path to the spring XML configuration file for the entire application. */
-  public static final String CONFIG_APPLICATION = "classpath:/resources/beans-application.xml";
+  public static final String CONFIG_APPLICATION = "classpath:/config/app/beans-application.xml";
 
   /** Path to the spring XML configuration file for the service layer. */
-  public static final String CONFIG_SERVICE = "classpath:/resources/service/beans-service.xml";
+  public static final String CONFIG_SERVICE = "classpath:/config/app/service/beans-service.xml";
 
   /** Path to the spring XML configuration file for the logic layer. */
-  public static final String CONFIG_LOGIC = "classpath:/resources/logic/beans-logic.xml";
+  public static final String CONFIG_LOGIC = "classpath:/config/app/logic/beans-logic.xml";
 
   /** Path to the spring XML configuration file for the persistence layer. */
-  public static final String CONFIG_PERSISTENCE = "classpath:/resources/persistence/beans-persistence.xml";
+  public static final String CONFIG_PERSISTENCE = "classpath:/config/app/persistence/beans-persistence.xml";
 
   /** Path to the spring XML configuration file for the common and cross-cutting code that is not assigned to any layer. */
-  public static final String CONFIG_COMMON = "classpath:/resources/common/beans-common.xml";
+  public static final String CONFIG_COMMON = "classpath:/config/app/common/beans-common.xml";
 
   /**
    * Construction prohibited.
