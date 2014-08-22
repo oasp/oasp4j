96b1bc5d91de15e6a132fa70bb65129f48220870
Joerg Hohwiller <joerg@j-hohwiller.de>
2014-08-22 09:42:58 +0200

    prepared for oasp4j-ide

---
+++ settings/ide-properties.bat
@@ -0,0 +1,6 @@
+@echo off
+rem ****************************************
+rem	Architect
+
+rem Variable replacement config
+set REPLACEMENT_PATTERNS_PATH=%SETTINGS_PATH%\eclipse\workspace\replacement-patterns.properties
