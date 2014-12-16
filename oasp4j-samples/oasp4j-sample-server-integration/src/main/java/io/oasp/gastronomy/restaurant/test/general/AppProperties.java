742dc8edd4ee95f867c6993d94a1525bceea2183
Joerg Hohwiller <joerg@j-hohwiller.de>
2014-12-16 21:35:21 +0100

    try 127.0.0.1 instead of localhost for error that only occurrs on jenkins, added some javadoc

--- oasp4j-samples/oasp4j-sample-server-integration/src/main/java/io/oasp/gastronomy/restaurant/test/general/AppProperties.java
+++ oasp4j-samples/oasp4j-sample-server-integration/src/main/java/io/oasp/gastronomy/restaurant/test/general/AppProperties.java
@@ -10,24 +10,16 @@ import java.util.Map;
  */
 public class AppProperties {
 
-  /**
-   *
-   */
-  public static final String host = "localhost";
+  /** The host of the server to test. */
+  public static final String host = "127.0.0.1";
 
-  /**
-   *
-   */
+  /** The port of the server to test. */
   public static final int port = 8081;
 
-  /**
-   *
-   */
+  /** The URL of the server to test. */
   public static final String hostUrl = "http://" + host + ":" + port + "/oasp4j-sample-server";
 
-  /**
-   *
-   */
+  /** The URL for the login. */
   public static final String loginUrl = hostUrl + "/j_spring_security_login";
 
   /**
@@ -37,49 +29,31 @@ public class AppProperties {
    */
   public static class LoginCredentials {
 
-    /**
-     *
-     */
+    /** Login of the default waiter. */
     public static final String waiterUsername = "waiter";
 
-    /**
-     *
-     */
+    /** Password of the default waiter. */
     public static final String waiterPassword = "waiter";
 
-    /**
-     *
-     */
+    /** Login of the default chief. */
     public static final String chiefUsername = "chief";
 
-    /**
-     *
-     */
+    /** Password of the default chief. */
     public static final String chiefPassword = "chief";
 
-    /**
-     *
-     */
+    /** Login of the default barkeeper. */
     public static final String barkeeperUsername = "barkeeper";
 
-    /**
-     *
-     */
+    /** Password of the default barkeeper. */
     public static final String barkeeperPassword = "barkeeper";
 
-    /**
-     *
-     */
+    /** Login of the default cook. */
     public static final String cookUsername = "cook";
 
-    /**
-     *
-     */
+    /** Password of the default cook. */
     public static final String cookPassword = "cook";
 
-    /**
-     *
-     */
+    /** {@link Map} from login to password. */
     public static final Map<String, String> usernamePasswordMapping;
     static {
       usernamePasswordMapping = new HashMap<>();
@@ -87,7 +61,6 @@ public class AppProperties {
       usernamePasswordMapping.put(chiefUsername, chiefPassword);
       usernamePasswordMapping.put(barkeeperUsername, barkeeperPassword);
       usernamePasswordMapping.put(cookUsername, cookPassword);
-
     }
 
   }
