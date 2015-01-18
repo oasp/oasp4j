fdbeb043c607c2142e0f928b9d2ab98ca34d9a94
Joerg Hohwiller <joerg@j-hohwiller.de>
2015-01-18 09:23:29 +0100

    #15: as a workaround used redundant java file until begin/end comment block feature has been improved

---
+++ oasp4j-templates/oasp4j-template-server/src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/general/common/api/constants/NamedQueries.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.general.common.api.constants;
+
+/**
+ * Constants of the named queries defined in <code>NamedQueries.xml</code>.
+ *
+ * @author hohwille
+ */
+public abstract class NamedQueries {
+
+  // put your query names from NamedQueries.xml as constants here (or generate with cobigen)
+
+}
