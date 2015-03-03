aac2edf469e8c0fb49c3f5e55769d5f9b6bf2c8e
Julian Metzler <julian.metzler@capgemini.com>
2015-03-03 13:46:36 +0100

    #190: audit all products

--- oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SideDishEntity.java
+++ oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SideDishEntity.java
@@ -5,6 +5,8 @@ import io.oasp.gastronomy.restaurant.offermanagement.common.api.SideDish;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+import org.hibernate.envers.Audited;
+
 /**
  * The {@link io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity persistent entity} for
  * {@link SideDish}.
@@ -13,6 +15,7 @@ import javax.persistence.Entity;
  */
 @Entity(name = "SideDish")
 @DiscriminatorValue("SideDish")
+@Audited
 public class SideDishEntity extends ProductEntity implements SideDish {
 
   private static final long serialVersionUID = 1L;
