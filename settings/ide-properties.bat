8bbffcb946e40d4187628f9b913890a7f1069c8e
Joerg Hohwiller <hohwille@users.sourceforge.net>
2015-07-28 09:13:18 +0200

    remove settings as announced, replaced by oasp4j-ide submodule (in oasp4j-ide-settings/src/main/settings)

--- settings/ide-properties.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem ****************************************
-rem	Architect
-
-rem Variable replacement config
-set REPLACEMENT_PATTERNS_PATH=%SETTINGS_PATH%\eclipse\workspace\replacement-patterns.properties
