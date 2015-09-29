4dd123f8d6661f8d7a375113f41fcaa0bf1e0ce2
Simon Spielmann <simon.spielmann@capgemini.com>
2015-09-29 14:03:44 +0200

    improve start scripts

--- oasp4j-samples/oasp4j-sample-batch/billExport.bat
+++ oasp4j-samples/oasp4j-sample-batch/billExport.bat
@@ -1,1 +1,9 @@
-call %~dp0\runbatch.bat billExportJob hello world
+@echo off
+call %~dp0\runbatch.bat billExportJob
+
+if errorlevel 0 goto success
+echo Failed.
+goto end
+:success
+echo Success.
+:end
