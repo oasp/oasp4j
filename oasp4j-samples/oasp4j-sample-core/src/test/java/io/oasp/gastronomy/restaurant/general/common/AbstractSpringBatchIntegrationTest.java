5d159a32ef9dd367fa17adefda45ae8dbd0fd23b
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2015-10-19 20:50:46 +0200

    #56 finished integration test

--- oasp4j-samples/oasp4j-sample-core/src/test/java/io/oasp/gastronomy/restaurant/general/common/AbstractSpringBatchIntegrationTest.java
+++ oasp4j-samples/oasp4j-sample-core/src/test/java/io/oasp/gastronomy/restaurant/general/common/AbstractSpringBatchIntegrationTest.java
@@ -1,13 +1,20 @@
 package io.oasp.gastronomy.restaurant.general.common;
 
+import static org.junit.Assert.fail;
+import io.oasp.gastronomy.restaurant.general.dataaccess.base.DatabaseMigrator;
+
+import java.io.File;
+import java.io.IOException;
+
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
@@ -23,6 +30,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 @ActiveProfiles("db-plain")
 public abstract class AbstractSpringBatchIntegrationTest {
+  private static final Logger LOG = LoggerFactory.getLogger(AbstractSpringBatchIntegrationTest.class);
+
+  private static final String TMP_DIR = "./tmp";
+
+  @Inject
+  protected DatabaseMigrator flyway;
 
   @Inject
   private JobLauncher jobLauncher;
@@ -37,5 +50,16 @@ public abstract class AbstractSpringBatchIntegrationTest {
     jobLauncherTestUtils.setJobLauncher(this.jobLauncher);
 
     return jobLauncherTestUtils;
+  }
+
+  @Before
+  public void setup() {
+
+    LOG.debug("Delete tmp directory");
+    try {
+      FileUtils.deleteDirectory(new File(TMP_DIR));
+    } catch (IOException e) {
+      fail();
+    }
   }
 }
--- oasp4j-samples/oasp4j-sample-core/src/test/java/io/oasp/gastronomy/restaurant/salesmanagement/batch/impl/billexport/BillExportJobTest.java
+++ oasp4j-samples/oasp4j-sample-core/src/test/java/io/oasp/gastronomy/restaurant/salesmanagement/batch/impl/billexport/BillExportJobTest.java
@@ -1,17 +1,24 @@
 package io.oasp.gastronomy.restaurant.salesmanagement.batch.impl.billexport;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import io.oasp.gastronomy.restaurant.general.common.AbstractSpringBatchIntegrationTest;
 import io.oasp.module.configuration.common.api.ApplicationConfigurationConstants;
 
+import java.io.File;
+
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
@@ -21,6 +28,7 @@ import org.springframework.test.context.ContextConfiguration;
  */
 @ContextConfiguration(locations = { ApplicationConfigurationConstants.BEANS_BATCH })
 public class BillExportJobTest extends AbstractSpringBatchIntegrationTest {
+  private static final Logger LOG = LoggerFactory.getLogger(BillExportJobTest.class);
 
   @Inject
   private Job billExportJob;
@@ -28,11 +36,25 @@ public class BillExportJobTest extends AbstractSpringBatchIntegrationTest {
   @Test
   public void testJob() throws Exception {
 
+    // setup test data
+    this.flyway.importTestData("classpath:BillExportJobTest/setup/db");
+
+    // configure job
+    File targetPath = new File("./tmp/bills.csv");
     JobParametersBuilder jobParameterBuilder = new JobParametersBuilder();
-    jobParameterBuilder.addString("bills.file", "./tmp/bills.csv");
+    jobParameterBuilder.addString("bills.file", targetPath.getPath());
     JobParameters jobParameters = jobParameterBuilder.toJobParameters();
 
+    // run job
     JobExecution jobExecution = getJobLauncherTestUtils(this.billExportJob).launchJob(jobParameters);
+
+    // check results
+    // - job status
     assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+
+    // - exported data
+    assertTrue(targetPath.exists());
+    assertTrue(FileUtils.contentEquals(targetPath,
+        new ClassPathResource("BillExportJobTest/expected/bills.csv").getFile()));
   }
 }
