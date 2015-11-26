b1c6d456fd8036f6dd63aec6754a7633b3bba0ad
pawel korzeniowski <pawel.korzeniowski@gmail.com>
2015-11-26 14:35:16 +0100

    Order in properties

--- oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/general/app/SpringBootApp.java
+++ oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/general/app/SpringBootApp.java
@@ -1,12 +1,14 @@
 package io.oasp.gastronomy.restaurant.general.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ManagementSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementSecurityAutoConfiguration.class })
 @ImportResource("config/app/beans-application.xml")
 public class SpringBootApp extends WebMvcConfigurerAdapter {
   public static void main(String[] args) {
