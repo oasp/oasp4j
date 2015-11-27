ef65fe67694bc06343d106a769048ce506a490bf
pawel korzeniowski <pawel.korzeniowski@gmail.com>
2015-11-27 16:48:07 +0100

    Fix

--- oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/general/app/SpringBootApp.java
+++ oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/general/app/SpringBootApp.java
@@ -3,12 +3,12 @@ package io.oasp.gastronomy.restaurant.general.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.ManagementSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementSecurityAutoConfiguration.class,
-DispatcherServletAutoConfiguration.class })
+HibernateJpaAutoConfiguration.class })
 @ImportResource("config/app/service/beans-service.xml")
 public class SpringBootApp {
 
