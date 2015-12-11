3402588aa1c8896b5efa0b02221f33f78bd61d92
Joerg Hohwiller <hohwille@users.sourceforge.net>
2015-12-11 01:32:58 +0100

    oasp/oasp4j#232: build bootified WAR, have main class in base package of app to follow spring-boot best practices and allow component scan of entire app

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/SpringBootApp.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/SpringBootApp.java
@@ -1,4 +1,4 @@
-package io.oasp.gastronomy.restaurant.general.app;
+package io.oasp.gastronomy.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/general/app/ServletInitializer.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/general/app/ServletInitializer.java
@@ -3,6 +3,8 @@ package io.oasp.gastronomy.restaurant.general.app;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+import io.oasp.gastronomy.restaurant.SpringBootApp;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
