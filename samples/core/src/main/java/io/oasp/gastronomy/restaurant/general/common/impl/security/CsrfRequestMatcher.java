eaa3f88395265078aa0ebf7c8b09e935ff4be2dd
Iwan van der Kleijn <ivanderk@capgemini.com>
2016-02-17 18:31:03 +0100

    Translation of German comments in CsrfRequestMatcher to English

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/general/common/impl/security/CsrfRequestMatcher.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/general/common/impl/security/CsrfRequestMatcher.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
- * Dies ist die Implementierung von {@link RequestMatcher}, die entscheidet, welche {@link HttpServletRequest Requests}
- * ein korrektes CSRF-Token erfordern.
+ * This is the implementation of {@link RequestMatcher}, which decides which {@link HttpServletRequest Requests} require
+ * a correct CSRF token.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Cross-site_request_forgery">Cross-site request forgery</a>
  *
  * @author hohwille
  */
@@ -16,20 +18,20 @@ public class CsrfRequestMatcher implements RequestMatcher {
 
   private static final Pattern HTTP_METHOD_PATTERN = Pattern.compile("^GET$");
 
-  private static final String[] PATH_PREFIXES_WITHOUT_CSRF_PROTECTION = { "/login", "/logout", "/services/rest/login", "/websocket" };
+  private static final String[] PATH_PREFIXES_WITHOUT_CSRF_PROTECTION =
+      { "/login", "/logout", "/services/rest/login", "/websocket" };
 
   @Override
   public boolean matches(HttpServletRequest request) {
 
-    // GET-Anfragen sind rein lesend und erfordern keinen CSRF-Schutz
+    // GET requests are read-only and therefore do not need CSRF protection
     if (HTTP_METHOD_PATTERN.matcher(request.getMethod()).matches()) {
 
       return false;
     }
 
-    // Spezielle URLs bzw. Anwendungen können nicht über das Portal vor CSRF geschützt werden und kümmern sich selbst
-    // darum. Das ist z.B. der Login selbst, da man das CSRF-Token erst nach dem Login abholt. Zum anderen sind das
-    // die externen Anwendungen Cadenza und Foswiki, die nicht durch Adamas beeinflusst werden können.
+    // There are specific URLs which can not be protected from CSRF. For example, in case of the the login page,
+    // the CSRF token can only be accessed after a successful authentication ("login").
     String requestPath = getRequestPath(request);
     for (String path : PATH_PREFIXES_WITHOUT_CSRF_PROTECTION) {
       if (requestPath.startsWith(path)) {
