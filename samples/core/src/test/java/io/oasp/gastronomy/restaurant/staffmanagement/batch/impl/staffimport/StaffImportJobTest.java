2f15a9a450b0588450927616ca59e48c725020f2
Röger <simon.roeger@capgemini.com>
2016-06-17 12:51:54 +0200

    added several assertThat's for StaffImportJobTest

--- samples/core/src/test/java/io/oasp/gastronomy/restaurant/staffmanagement/batch/impl/staffimport/StaffImportJobTest.java
+++ samples/core/src/test/java/io/oasp/gastronomy/restaurant/staffmanagement/batch/impl/staffimport/StaffImportJobTest.java
@@ -47,48 +47,25 @@ public class StaffImportJobTest extends AbstractSpringBatchIntegrationTest {
   @Test
   public void testJob() throws Exception {
 
-    // prepare database - migrate all data but erase offers
+    // prepare database
     this.flyway.clean();
     this.flyway.migrate();
-    cleanOffers();
 
-    // configure job
+    // given
     JobParametersBuilder jobParameterBuilder = new JobParametersBuilder();
     jobParameterBuilder.addString("pathToFile", "staffImportJobTest/staff.csv");
     JobParameters jobParameters = jobParameterBuilder.toJobParameters();
 
-    // run job
+    // when
     JobExecution jobExecution = getJobLauncherTestUtils(this.staffImportJob).launchJob(jobParameters);
 
-    // check results
-    // - job status
+    // then
     assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
 
     StaffMemberEto testMember = this.staffmanagement.findStaffMember(100L);
-    System.out.println(testMember.getName() + " " + testMember.getRole().getName());
+    assertThat(testMember.getRole().getName()).isEqualTo("Cook");
+    assertThat(this.staffmanagement.findAllStaffMembers().size()).isEqualTo(2003);
 
-    //
-    // // - imported data (there are 3 offers in setup data)
-    // List<OfferEto> allOffers = this.offermanagement.findAllOffers();
-    // assertThat(allOffers).hasSize(3);
-    //
-    // // - exemplary offer
-    // OfferEto offer = allOffers.get(0);
-    // assertThat(offer.getName()).isEqualTo("Leckeres-Menü");
-    // assertThat(offer.getDescription()).isEqualTo("Description of Leckeres-Menü");
-    // assertThat(offer.getPrice()).isEqualTo(new Money(15.99));
-    // assertThat(offer.getState()).isEqualTo(OfferState.NORMAL);
-  }
-
-  /**
-   * @param staffImportJob2
-   * @return
-   */
-
-  private void cleanOffers() {
-
-    // for (OfferEto offer : this.staffmanagement.findAllOffers()) {
-    // this.staffmanagement.deleteOffer(offer.getId());
   }
 
 }
