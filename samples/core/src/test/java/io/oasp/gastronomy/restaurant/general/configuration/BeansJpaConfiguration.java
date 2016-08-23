759f681c672fb81f7aac87390e025814c795ed9a
Huber <simon.huber@capgemini.com>
2016-08-23 14:47:27 +0200

    unused BeansJpaConfiguration.EntityManagerFactory deleted

--- samples/core/src/test/java/io/oasp/gastronomy/restaurant/general/configuration/BeansJpaConfiguration.java
+++ samples/core/src/test/java/io/oasp/gastronomy/restaurant/general/configuration/BeansJpaConfiguration.java
@@ -1,15 +1,14 @@
 package io.oasp.gastronomy.restaurant.general.configuration;
 
-import io.oasp.gastronomy.restaurant.general.dataaccess.base.DatabaseMigrator;
-
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.oasp.gastronomy.restaurant.general.dataaccess.base.DatabaseMigrator;
 
 /**
  * Java configuration for JPA
@@ -19,8 +18,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 // @EnableTransactionManagement
 public class BeansJpaConfiguration {
-
-  private @Autowired EntityManagerFactory entityManagerFactory;
 
   private @Autowired DataSource appDataSource;
 
