ecceeac010316cdf27a59942befb7a41a5d8f9c3
Jonas Molina Ramirez <jonas.molina-ramirez@capgemini.com>
2016-10-28 11:15:02 +0200

    Added static variable to BaseTest to determine if class has already been set up

--- modules/test/src/main/java/io/oasp/module/test/common/base/BaseTest.java
+++ modules/test/src/main/java/io/oasp/module/test/common/base/BaseTest.java
@@ -34,6 +34,11 @@ import org.junit.Before;
  * @author shuber, jmolinar
  */
 public abstract class BaseTest extends Assertions {
+  /**
+   * Indicates if the test class is to be set up for the first time. {@code true} indicates that the class has already
+   * been set up (e.g., database setup) for the execution of an preceding test method.
+   */
+  protected static boolean INITIALIZED = false;
 
   /**
    * Suggests to use {@link #doSetUp()} method before each tests.
@@ -41,7 +46,11 @@ public abstract class BaseTest extends Assertions {
   @Before
   public final void setUp() {
 
-    doSetUp();
+    // Simply sets INITIALIZED to true when setUp is called for the first time.
+    if (!INITIALIZED) {
+      INITIALIZED = true;
+    }
+    doSetUp(INITIALIZED);
   }
 
   /**
@@ -56,7 +65,7 @@ public abstract class BaseTest extends Assertions {
   /**
    * Provides initialization previous to the creation of the text fixture.
    */
-  protected void doSetUp() {
+  protected void doSetUp(boolean initialized) {
 
   }
 
