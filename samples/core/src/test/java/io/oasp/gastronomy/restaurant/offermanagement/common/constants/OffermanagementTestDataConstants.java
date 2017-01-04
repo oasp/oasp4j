de21d551ec1c3e5bcf89b0a2d264fb80577121f2
kiran-vadla <vadla.kiran@capgemini.com>
2017-01-04 19:40:32 +0530

    Refactor and submit the code submitted as part of PR#487 onto a new branch 'sampleValueCreation' under develop-2.3.0'

---
+++ samples/core/src/test/java/io/oasp/gastronomy/restaurant/offermanagement/common/constants/OffermanagementTestDataConstants.java
@@ -0,0 +1,23 @@
+package io.oasp.gastronomy.restaurant.offermanagement.common.constants;
+
+/**
+ * @author vkiran
+ *
+ */
+public class OffermanagementTestDataConstants {
+  public static final long ID_OFFER_SCHNITZELMENUE = 1L;
+
+  public static final long ID_PRODUCT_MEAL_SCHNITZEL = 1L;
+
+  public static final long ID_PRODUCT_SIDEDISH_POMMES = 7L;
+
+  public static final long ID_PRODUCT_DRINK_COLA = 12L;
+
+  public static final long NUMBER_OF_PRODUCT_MEAL = 6L;
+
+  public static final long NUMBER_OF_PRODUCT_SIDEDISH = 4L;
+
+  public static final long NUMBER_OF_PRODUCT_DRINK = 4L;
+
+  public static final long BILL_ID = 1L;
+}
