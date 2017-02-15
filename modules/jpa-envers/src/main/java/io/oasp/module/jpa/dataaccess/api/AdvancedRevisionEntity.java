24d5097906307c390a77372527cd7af1debd99e1
Mathieu Lavigne <mathieu.lavigne@capgemini.com>
2017-02-15 17:28:41 +0100

    merge

--- modules/jpa-envers/src/main/java/io/oasp/module/jpa/dataaccess/api/AdvancedRevisionEntity.java
+++ modules/jpa-envers/src/main/java/io/oasp/module/jpa/dataaccess/api/AdvancedRevisionEntity.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Column;
 
 import net.sf.mmm.util.entity.api.PersistenceEntity;
 
