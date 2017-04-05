0c21ae1ae548c13e42aa42278d90c2d582581dbf
kiran-vadla <vadla.kiran@capgemini.com>
2017-04-05 11:33:50 +0530

    Added scripts to  disable other databases and enable h2 Database

---
+++ samples/core/src/test/setup/disablemssqldb.sh
@@ -0,0 +1,21 @@
+mv ../../main/resources/db/migration/mssql/V0001__R001_Create_schema_MSSQL.sql ../../main/resources/db/migration/mssql/V0001__R001_Create_schema_MSSQL.sql.mssql
+mv ../../main/resources/db/migration/h2/V0001__R001_Create_schema.sql.h2 ../../main/resources/db/migration/h2/V0001__R001_Create_schema.sql
+mv ../../main/resources/db/migration/mssql/V0002__R001_Master_data_MSSQL.sql ../../main/resources/db/migration/mssql/V0002__R001_Master_data_MSSQL.sql.mssql
+mv ../../main/resources/db/migration/V0002__R001_Master_data.sql.h2 ../../main/resources/db/migration/V0002__R001_Master_data.sql
+mv ../../main/resources/db/migration/mssql/V0003__R001_Add_blob_table_and_data_MSSQL.sql ../../main/resources/db/migration/mssql/V0003__R001_Add_blob_table_and_data_MSSQL.sql.mssql
+mv ../../main/resources/db/migration/V0003__R001_Add_blob_table_and_data.sql.h2 ../../main/resources/db/migration/V0003__R001_Add_blob_table_and_data.sql
+mv ../../main/resources/db/migration/mssql/V0004__R001_Add_batch_tables_MSSQL.sql ../../main/resources/db/migration/mssql/V0004__R001_Add_batch_tables_MSSQL.sql.mssql
+mv ../../main/resources/db/migration/V0004__R001_Add_batch_tables.sql.h2 ../../main/resources/db/migration/V0004__R001_Add_batch_tables.sql
+mv ../../test/resources/AllTests/setup/db/V9001_1__Delete_data.sql.h2 ../../test/resources/AllTests/setup/db/V9001_1__Delete_data.sql
+mv ../../test/resources/AllTests/setup/db/V9001_1__Delete_data_MSSQL.sql ../../test/resources/AllTests/setup/db/V9001_1__Delete_data_MSSQL.sql.mssql
+mv ../../test/resources/BillExportJobTest/setup/db/V9901_1__Import_data.sql.h2 ../../test/resources/BillExportJobTest/setup/db/V9901_1__Import_data.sql
+mv ../../test/resources/BillExportJobTest/setup/db/V9901_1__Import_data_MSSQL.sql ../../test/resources/BillExportJobTest/setup/db/V9901_1__Import_data_MSSQL.sql.mssql
+mv ../../test/resources/db/tablemanagement/V0001__R001_Create_schema.sql.h2 ../../test/resources/db/tablemanagement/V0001__R001_Create_schema.sql
+mv ../../test/resources/db/tablemanagement/V0001__R001_Create_schema_MSSQL.sql ../../test/resources/db/tablemanagement/V0001__R001_Create_schema_MSSQL.sql.mssql
+mv ../../test/resources/db/tablemanagement/V0002__R001_Master_data.sql.h2 ../../test/resources/db/tablemanagement/V0002__R001_Master_data.sql
+mv ../../test/resources/db/tablemanagement/V0002__R001_Master_data_MSSQL.sql ../../test/resources/db/tablemanagement/V0002__R001_Master_data_MSSQL.sql.mssql
+mv ../../test/resources/db/tablemanagement/V0003__R001_Add_blob_table_and_data.sql.h2 ../../test/resources/db/tablemanagement/V0003__R001_Add_blob_table_and_data.sql
+mv ../../test/resources/db/tablemanagement/V0003__R001_Add_blob_table_and_data_MSSQL.sql ../../test/resources/db/tablemanagement/V0003__R001_Add_blob_table_and_data_MSSQL.sql.mssql
+mv ../../test/resources/db/tablemanagement/V0004__R001_Add_batch_tables.sql.h2 ../../test/resources/db/tablemanagement/V0004__R001_Add_batch_tables.sql
+mv ../../test/resources/db/tablemanagement/V0004__R001_Add_batch_tables_MSSQL.sql ../../test/resources/db/tablemanagement/V0004__R001_Add_batch_tables_MSSQL.sql.mssql
+
