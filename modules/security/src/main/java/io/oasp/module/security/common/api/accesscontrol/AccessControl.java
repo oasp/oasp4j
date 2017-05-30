36187777b6985281b5f7481204b3f7db8f8be750
kiran-vadla <vadla.kiran@capgemini.com>
2017-05-30 14:35:12 +0530

    Deleted the deprecated classes of security module

--- modules/security/src/main/java/io/oasp/module/security/common/api/accesscontrol/AccessControl.java
+++ modules/security/src/main/java/io/oasp/module/security/common/api/accesscontrol/AccessControl.java
@@ -3,15 +3,14 @@ package io.oasp.module.security.common.api.accesscontrol;
 /**
  * This is the interface for a node of the {@link AccessControlSchema} that represents a tree of
  * {@link AccessControlGroup}s and {@link AccessControlPermission}s. If a {@link java.security.Principal} "has" a
- * {@link AbstractAccessControl} he also "has" all {@link AbstractAccessControl}s with according permissions in the
- * spanned sub-tree.
+ * {@link AccessControl} he also "has" all {@link AccessControl}s with according permissions in the spanned sub-tree.
  *
  */
 public interface AccessControl {
 
   /**
-   * @return the unique identifier of {@link AbstractAccessControl}. Has to be unique for all
-   *         {@link AbstractAccessControl} in a {@link AccessControlSchema}.
+   * @return the unique identifier of {@link AccessControl}. Has to be unique for all {@link AccessControl} in a
+   *         {@link AccessControlSchema}.
    */
   public String getId();
 }
