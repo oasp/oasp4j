6cdd808dfc7e35cf84c2455a79b44e8ce1f83670
Joerg Hohwiller <hohwille@users.sourceforge.net>
2017-10-04 11:25:14 +0200

    oasp/oasp4j#358: fixed damn stupid copy-paste bug

--- modules/cxf-client-ws/src/main/java/io/oasp/module/cxf/common/impl/client/ws/CxfWsClientAutoConfiguration.java
+++ modules/cxf-client-ws/src/main/java/io/oasp/module/cxf/common/impl/client/ws/CxfWsClientAutoConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import io.oasp.module.service.common.api.sync.SyncServiceClientFactory;
 
 /**
- * {@link Configuration} for REST (JAX-RS) clients using Apache CXF.
+ * {@link Configuration} for SOAP (JAX-WS) clients using Apache CXF.
  *
  * @since 3.0.0
  */
@@ -14,10 +14,10 @@ import io.oasp.module.service.common.api.sync.SyncServiceClientFactory;
 public class CxfWsClientAutoConfiguration {
 
   /**
-   * @return an implemenation of {@link SyncServiceClientFactory} based on CXF for REST (JAX-RS).
+   * @return an implemenation of {@link SyncServiceClientFactory} based on CXF for SAOP (JAX-WS).
    */
   @Bean
-  public SyncServiceClientFactory syncServiceClientFactoryCxfRest() {
+  public SyncServiceClientFactory syncServiceClientFactoryCxfWs() {
 
     return new SyncServiceClientFactoryCxfWs();
   }
