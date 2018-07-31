3dfdb64a09e80342ed138338d6c881ab0220aa28
Joerg Hohwiller <hohwille@users.sourceforge.net>
2018-07-31 15:05:47 +0200

    #627: changed template to use spring-data repository instead of DAO

---
+++ templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/dataaccess/api/dao/BinaryObjectRepository.java
@@ -0,0 +1,11 @@
+package ${package}.general.dataaccess.api.dao;
+
+import io.oasp.module.jpa.dataaccess.api.data.DefaultRepository;
+import ${package}.general.dataaccess.api.BinaryObjectEntity;
+
+/**
+ * {@link DefaultRepository} for {@link BinaryObjectEntity}.
+ */
+public interface BinaryObjectRepository extends DefaultRepository<BinaryObjectEntity> {
+
+}
