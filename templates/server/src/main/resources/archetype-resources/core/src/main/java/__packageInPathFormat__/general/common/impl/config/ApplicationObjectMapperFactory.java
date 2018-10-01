4d4d2c3c1d242b50683e17e6002a0a8b1828116e
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2018-10-01 17:51:19 +0200

    Adding Pageable deserializer to archetype

--- templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/config/ApplicationObjectMapperFactory.java
+++ templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/config/ApplicationObjectMapperFactory.java
@@ -2,7 +2,11 @@ package ${package}.general.common.impl.config;
 
 import javax.inject.Named;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.web.csrf.CsrfToken;
+
+import com.devonfw.poc.angular.general.common.api.to.PageableJsonDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import io.oasp.module.json.common.base.ObjectMapperFactory;
 
@@ -20,5 +24,8 @@ public class ApplicationObjectMapperFactory extends ObjectMapperFactory {
     super();
     // register polymorphic mapping here - see https://github.com/oasp-forge/oasp4j-wiki/wiki/guide-json#json-and-inheritance
     getExtensionModule().addAbstractTypeMapping(CsrfToken.class, CsrfTokenImpl.class);
+	// register Pageable deserializer
+    SimpleModule module = getExtensionModule();
+    module.addDeserializer(Pageable.class, new PageableJsonDeserializer());
   }
 }
